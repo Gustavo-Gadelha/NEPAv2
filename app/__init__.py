02e5c42fcc405731410cbd25811d05e43a317a68
Gustavo-Gadelha <gustavofg2112@gmail.com>
2026-05-06 03:08:02 -0300

    feat: implementa CORS com flask_cors

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from dotenv import load_dotenv
 from flask import Flask
+from flask_cors import CORS
 
 from app.extensions import init_extensions
 from app.healthcheck import register_healthcheck
@@ -17,6 +18,10 @@ def create_app(settings_module: str = ''):
         app.config.from_object(settings)
     except ImportError as e:
         raise RuntimeError('FLASK_SETTINGS_MODULE was set improperly') from e
+
+    # See configuration reference:
+    # https://flask-cors.readthedocs.io/en/latest/configuration.html#
+    CORS(app)
 
     init_extensions(app)
     register_healthcheck(app)
