1642a508c863c6210d17f618a98ba42583a22c8d
Gustavo-Gadelha <gustavofg2112@gmail.com>
2026-05-06 02:44:55 -0300

    feat: migrações de banco usando Alembic com Flask-Migrate

--- app/extensions.py
+++ app/extensions.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 from app.database import Base
 
 db = SQLAlchemy(model_class=Base, disable_autonaming=True)
+migrate = Migrate()
 
 
 def init_extensions(app: Flask):
     db.init_app(app)
+    migrate.init_app(app, db)
