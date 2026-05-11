ca91cc771cab5f013fa36968b546b74b455b601c
Gustavo-Gadelha <gustavofg2112@gmail.com>
2026-05-11 02:22:04 -0300

    chore: remove configurações de debug de errors

--- app/features/errors/__init__.py
+++ app/features/errors/__init__.py
@@ -3,8 +3,6 @@ from flask import abort
 from app.engine import Feature
 
 errors = Feature('errors', __name__, verbose_name='Errors')
-errors.add_url_rule('/error/404', '404', view_func=lambda: abort(404))
-errors.add_url_rule('/error/500', '500', view_func=lambda: abort(500))
 
 
 @errors.on_load
